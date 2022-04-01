5bacd994ae4ddd05dbf62e5dcf37500fa50d8e25
Vitall <temmets20@gmail.com>
2022-04-01 22:18:09 +0700

    tst

--- js/ant.js
+++ js/ant.js
@@ -1,2 +1,30 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const startBtn = document.getElementById("btn-start")
+const clearBtn = document.getElementById("btn-clear")
+
+var dotHeap = []
+
+
+
+
+
+
+function clearArea(){
+    dotHeap = []
+    ctx.beginPath();
+    ctx.rect(0,0,1000,700)
+    ctx.fillStyle = "white"
+    ctx.fill();
+}
+
+
+function start(){
+    
+}
+
+
+
+//events
+startBtn.addEventListener("click", start)
+clearBtn.addEventListener("click", clearArea)
